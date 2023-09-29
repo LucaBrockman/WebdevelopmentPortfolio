cddbdb3a1b1f7cf3edb365887f9709da138fb260
Luca Brockman <luca.brockman@student.nhlstenden.com>
2023-09-29 23:11:52 +0200

    header fix

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header>
-        <a href="#index">Home</a>
+        <a href="index.php">Home</a>
         <ul id="navbar">
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="php.php">PHP</a></li>
+            <li><a href="index.php#portfolio">Portfolio</a></li>
+            <li><a href="index.php#about">About</a></li>
+            <li><a href="index.php#contact">Contact</a></li>
+            <li><a href="index.php#php.php">PHP</a></li>
         </ul>
 </header>
